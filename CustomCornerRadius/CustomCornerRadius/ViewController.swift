a63365b2f06ab1d639e65cbef79d97224f0fd7ba
shsx477 <shsx477@gmail.com>
2020-06-01 19:59:10 +0900

    custom rectangle corner radius

---
+++ CustomCornerRadius/CustomCornerRadius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCornerRadius
+//
+//  Created by 한선수 on 2020/06/01.
+//  Copyright © 2020 한선수. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
